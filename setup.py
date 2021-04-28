7515ddcd66fb627d9a591db33877dbd6d4234830
shresth_singh <singhshresth26@gmail.com>
2021-04-28 18:36:01 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='nifpy',
@@ -11,7 +15,8 @@ setup(
                         index list and fundamentals such as income statement, cash flow statement
                         and balance sheet.
                   """,
-
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license = 'MIT',
     packages = ['nifpy'],
     keywords = ['stocks', 'nifty', 'financials','algo trading'],
