9726300419ed071a59fdafde024dd4cf5082d770
shresth26 <singhshresth26@gmail.com>
2023-11-15 23:13:06 +0530

    Removed class from info module

--- nifpy/info.py
+++ nifpy/info.py
@@ -4,148 +4,132 @@ import yfinance as web
 import numpy as np
 
 
-class stockPrice:
+# todo append .NS in the end of tickers if they don't exist already
 
-    #todo append .NS in the end of tickers if they don't exist already
-    @staticmethod
-    def get_nifty() -> pd.DataFrame:
+def get_closing_data(tickers, start=TODAY - PREV, end=TODAY) -> pd.DataFrame:
+    """
 
-        """
-            This function returns the list of all the Nifty tickers.
-        """
+        This function returns the Closing price of a list of tickers mentioned in the parameter.
+        Functions that are defined above can be directly passed to get the closing price of
+        each ticker in the index
 
-        nifty = pd.read_html(BASE_URL)[2]
-        return nifty
+        Parameters
+        --------------------------------
 
-    @staticmethod
-    def get_closing_data(tickers, start=TODAY - PREV, end=TODAY) -> pd.DataFrame:
+        tickers : Contains a list of symbols for which the closing price will be returned
 
-        """
+        start :   Contains the starting date from which closing price is required
+                  Format: 'dd/mm/yyyy' as in '25/04/2021'
+                  Default: Three months from today's date
 
-            This function returns the Closing price of a list of tickers mentioned in the parameter.
-            Functions that are defined above can be directly passed to get the closing price of
-            each ticker in the index
+        end :     Contains the end date till which closing price is required
+                  Format: 'dd/mm/yyyy' as in '27/04/2021'
+                  Default: Today's date
 
-            Parameters
-            --------------------------------
+        Other than a custom list some other parameters that can be passed directly are:
 
-            tickers : Contains a list of symbols for which the closing price will be returned
+        Returns
+        --------------------------------
 
-            start :   Contains the starting date from which closing price is required
-                      Format: 'dd/mm/yyyy' as in '25/04/2021'
-                      Default: Three months from today's date
+        A pandas dataframe that contains the closing price of all symbols passed as the
+        parameter to the function.
 
-            end :     Contains the end date till which closing price is required
-                      Format: 'dd/mm/yyyy' as in '27/04/2021'
-                      Default: Today's date
+    """
 
-            Other than a custom list some other parameters that can be passed directly are:
+    Closing = pd.DataFrame()
+    for i in range(len(tickers)):
+        try:
+            temp = web.download(tickers[i], start=start, end=end)
+            temp.dropna(inplace=True)
+            Closing[tickers[i]] = temp['Close']
+        except:
+            print("No info is available for this particular stock " + tickers[i])
+    return Closing
 
-            Returns
-            --------------------------------
 
-            A pandas dataframe that contains the closing price of all symbols passed as the
-            parameter to the function.
+def get_stock_data(ticker, start=TODAY - PREV, end=TODAY) -> pd.DataFrame:
+    """
 
-        """
+        This function returns the various attributes of a ticker such as the High, Low,
+        Open, Close, Volume and Adjusted Close
 
-        Closing = pd.DataFrame()
-        for i in range(len(tickers)):
-            try:
-                temp = web.download(tickers[i], start=start, end=end)
-                temp.dropna(inplace=True)
-                Closing[tickers[i]] = temp['Close']
-            except:
-                print("No info is available for this particular stock " + tickers[i])
-        return Closing
+        Parameters
+        --------------------------------
 
-    @staticmethod
-    def get_stock_data(ticker, start=TODAY - PREV, end=TODAY) -> pd.DataFrame:
+        ticker : Contains the symbol/ticker for which various attributes will be returned
 
-        """
+        start :   Contains the starting date
+                  Format: 'dd/mm/yyyy' as in '25/04/2021'
+                  Default: Three months from today's date
 
-            This function returns the various attributes of a ticker such as the High, Low,
-            Open, Close, Volume and Adjusted Close
+        end :     Contains the end date
+                  Format: 'dd/mm/yyyy' as in '27/04/2021'
+                  Default: Today's date
 
-            Parameters
-            --------------------------------
+        Returns
+        --------------------------------
 
-            ticker : Contains the symbol/ticker for which various attributes will be returned
+        A pandas dataframe that contains various attributes of a ticker such as the High, Low,
+        Open, Close, Volume and Adjusted Close
 
-            start :   Contains the starting date
-                      Format: 'dd/mm/yyyy' as in '25/04/2021'
-                      Default: Three months from today's date
+    """
 
-            end :     Contains the end date
-                      Format: 'dd/mm/yyyy' as in '27/04/2021'
-                      Default: Today's date
+    temp = web.download(ticker, start=start, end=end)
+    return temp
 
-            Returns
-            --------------------------------
 
-            A pandas dataframe that contains various attributes of a ticker such as the High, Low,
-            Open, Close, Volume and Adjusted Close
+def live_price(ticker) -> float:
+    """
 
-        """
+        This function returns the live/latest price for the symbol that has been passed as the
+        parameter.
 
-        temp = web.download(ticker, start=start, end=end)
-        return temp
+        Parameters
+        --------------------------------
+        ticker : Contains the symbol/ticker for which the live price will be returned
 
-    @staticmethod
-    def live_price(ticker) -> float:
+    """
 
-        """
+    temp = web.download(ticker, TODAY - PREV)['Close']
+    return np.round(temp[-1], 2)
 
-            This function returns the live/latest price for the symbol that has been passed as the
-            parameter.
 
-            Parameters
-            --------------------------------
-            ticker : Contains the symbol/ticker for which the live price will be returned
+def stock_summary(symbol) -> pd.DataFrame:
+    """
 
-        """
+        This function returns the summary of various attributes of the symbol/ticker that
+        has been passed as the parameter.
 
-        temp = web.download(ticker, TODAY - PREV)['Close']
-        return np.round(temp[-1], 2)
+        Parameters
+        --------------------------------
 
-    @staticmethod
-    def stock_summary(symbol) -> pd.DataFrame:
+        symbol : Contains the symbol/ticker for which the summary of various attributes
+                  will be returned
 
-        """
+        Returns
+        --------------------------------
 
-            This function returns the summary of various attributes of the symbol/ticker that
-            has been passed as the parameter.
+        A pandas dataframe that contains various attributes of a ticker such as the:
+        - Previous Close
+        - Open
+        - Bid
+        - Ask
+        - Day's Range
+        - 52 Week Range
+        - Volume
+        - Average Volume
+        - Market Cap
+        - Beta
+        - P/E Ratio
+        - EPS
+        - Earnings Date
+        - Forward Dividend and Yield
+        - Ex-Dividend Date
+        - 1 Year Target Estimate
 
-            Parameters
-            --------------------------------
+    """
 
-            symbol : Contains the symbol/ticker for which the summary of various attributes
-                      will be returned
-
-            Returns
-            --------------------------------
-
-            A pandas dataframe that contains various attributes of a ticker such as the:
-            - Previous Close
-            - Open
-            - Bid
-            - Ask
-            - Day's Range
-            - 52 Week Range
-            - Volume
-            - Average Volume
-            - Market Cap
-            - Beta
-            - P/E Ratio
-            - EPS
-            - Earnings Date
-            - Forward Dividend and Yield
-            - Ex-Dividend Date
-            - 1 Year Target Estimate
-
-        """
-
-        link = pd.read_html('https://finance.yahoo.com/quote/' + symbol + '?p=' + symbol)
-        link1 = pd.concat([link[0], link[1]], ignore_index=True)
-        link1.columns = ['Attribute', 'Value']
-        return link1
+    link = pd.read_html('https://finance.yahoo.com/quote/' + symbol + '?p=' + symbol)
+    link1 = pd.concat([link[0], link[1]], ignore_index=True)
+    link1.columns = ['Attribute', 'Value']
+    return link1
